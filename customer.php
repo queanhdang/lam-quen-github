dc2e7a77c4a117fe36e012e339faa6fb0f80b4ae
dangqueanh <dqa.d16ptit@gmail.com>
2020-08-31 09:40:31 +0700

    xac nhap featurenhanh1

--- customer.php
+++ customer.php
@@ -3,10 +3,16 @@ class customer
 {
     public $name;
     public $color;
-    public __constructor()
+    public function set_name($name) 
     {
-        $this->name = "DQA";
-        $this->color = "blue";
+        $this->name = $name;
     }
-
+    public function get_name() 
+    {
+        return $this->name;
+    }
 }
+
+$c = new customer();
+$c->set_name("DQA");
+echo $c->get_name();
